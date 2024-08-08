c6c5e6f51272ff990777df1fed8f5cfd16298cd0
Yoann Laissus <yoann.laissus@jeresiliemoncontrat.com>
2024-08-08 12:40:27 +0200

    Get rid of legacy assert_* functions

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,215 +6,147 @@
 #    Array of plugin sub-packages to install. Default: empty
 #
 class dovecot (
-    $plugins                    = [],
+    Array $plugins                    = [],
     # dovecot.conf
-    $protocols                  = undef,
-    $listen                     = undef,
-    $login_greeting             = undef,
-    $login_trusted_networks     = undef,
-    $verbose_proctitle          = undef,
+    String $protocols                  = undef,
+    Optional[String] $listen                     = undef,
+    Optional[String] $login_greeting             = undef,
+    Optional[String] $login_trusted_networks     = undef,
+    Optional[String] $verbose_proctitle          = undef,
     $shutdown_clients           = undef,
     # 10-auth.conf
-    $disable_plaintext_auth     = true,
-    $auth_username_chars        = undef,
-    $auth_mechanisms            = 'plain',
-    $auth_include               = [ 'system' ],
+    Boolean $disable_plaintext_auth     = true,
+    Optional[String] $auth_username_chars        = undef,
+    String $auth_mechanisms            = 'plain',
+    Array $auth_include               = [ 'system' ],
     # 10-logging.conf
-    $log_path                   = undef,
-    $log_timestamp              = undef,
-    $auth_verbose               = undef,
-    $auth_debug                 = undef,
-    $mail_debug                 = undef,
+    Optional[String] $log_path                   = undef,
+    Optional[String] $log_timestamp              = undef,
+    Optional[String] $auth_verbose               = undef,
+    Optional[String] $auth_debug                 = undef,
+    Optional[String] $mail_debug                 = undef,
     # 10-mail.conf
-    $mail_home                  = undef,
-    $mail_fsync                 = undef,
-    $mail_location              = undef,
-    $mail_uid                   = undef,
-    $mail_gid                   = undef,
-    $mail_nfs_index             = undef,
-    $mail_nfs_storage           = undef,
-    $mail_privileged_group      = undef,
-    $mail_plugins               = undef,
-    $mmap_disable               = undef,
-    $dotlock_use_excl           = undef,
-    $include_inbox_namespace    = undef,
+    Optional[String] $mail_home                  = undef,
+    Optional[String] $mail_fsync                 = undef,
+    Optional[String] $mail_location              = undef,
+    Optional[String] $mail_uid                   = undef,
+    Optional[String] $mail_gid                   = undef,
+    Optional[String] $mail_nfs_index             = undef,
+    Optional[String] $mail_nfs_storage           = undef,
+    Optional[String] $mail_privileged_group      = undef,
+    Optional[String] $mail_plugins               = undef,
+    Optional[String] $mmap_disable               = undef,
+    Optional[String] $dotlock_use_excl           = undef,
+    Boolean $include_inbox_namespace    = false,
     # 10-master.conf
-    $default_process_limit      = undef,
-    $default_client_limit       = undef,
-    $auth_listener_userdb_mode   = undef,
-    $auth_listener_userdb_user   = undef,
-    $auth_listener_userdb_group  = undef,
-    $auth_listener_postfix       = false,
-    $auth_listener_postfix_mode  = undef,
-    $auth_listener_postfix_user  = undef,
-    $auth_listener_postfix_group = undef,
-    $imap_login_process_limit   = undef,
-    $imap_login_client_limit    = undef,
-    $imap_login_service_count   = undef,
-    $imap_login_process_min_avail = undef,
-    $imap_login_vsz_limit       = undef,
-    $pop3_login_service_count   = undef,
-    $pop3_login_process_min_avail = undef,
-    $default_vsz_limit          = undef,
-    $auth_listener_default_user = undef,
-    $imaplogin_imap_port         = 0,
-    $imaplogin_imaps_port        = 0,
-    $imaplogin_imaps_ssl         = false,
-    $lmtp_unix_listener          = undef,
-    $lmtp_unix_listener_mode     = undef,
-    $lmtp_unix_listener_user     = undef,
-    $lmtp_unix_listener_group    = undef,
-    $lmtp_socket_group          = undef,
-    $lmtp_socket_mode           = undef,
-    $lmtp_socket_path           = undef,
-    $lmtp_socket_user           = undef,
+    Optional[String] $default_process_limit      = undef,
+    Optional[String] $default_client_limit       = undef,
+    Optional[String] $auth_listener_userdb_mode   = undef,
+    Optional[String] $auth_listener_userdb_user   = undef,
+    Optional[String] $auth_listener_userdb_group  = undef,
+    Boolean $auth_listener_postfix       = false,
+    Optional[String] $auth_listener_postfix_mode  = undef,
+    Optional[String] $auth_listener_postfix_user  = undef,
+    Optional[String] $auth_listener_postfix_group = undef,
+    Optional[String] $imap_login_process_limit   = undef,
+    Optional[String] $imap_login_client_limit    = undef,
+    Optional[String] $imap_login_service_count   = undef,
+    Optional[String] $imap_login_process_min_avail = undef,
+    Optional[String] $imap_login_vsz_limit       = undef,
+    Optional[String] $pop3_login_service_count   = undef,
+    Optional[String] $pop3_login_process_min_avail = undef,
+    Optional[String] $default_vsz_limit          = undef,
+    Optional[String] $auth_listener_default_user = undef,
+    Integer $imaplogin_imap_port         = 0,
+    Integer $imaplogin_imaps_port        = 0,
+    Boolean $imaplogin_imaps_ssl         = false,
+    Optional[String] $lmtp_unix_listener          = undef,
+    Optional[String] $lmtp_unix_listener_mode     = undef,
+    Optional[String] $lmtp_unix_listener_user     = undef,
+    Optional[String] $lmtp_unix_listener_group    = undef,
+    Optional[String] $lmtp_socket_group          = undef,
+    Optional[String] $lmtp_socket_mode           = undef,
+    Optional[String] $lmtp_socket_path           = undef,
+    Optional[String] $lmtp_socket_user           = undef,
     # 10-ssl.conf
-    $ssl                        = undef,
-    $ssl_cert                   = '/etc/pki/dovecot/certs/dovecot.pem',
-    $ssl_key                    = '/etc/pki/dovecot/private/dovecot.pem',
-    $ssl_cipher_list            = undef,
-    $ssl_protocols              = undef,
-    $ssl_dh_parameters_length   = undef,
-    $ssl_dh                     = '/usr/share/dovecot/dh.pem',
+    Optional[String] $ssl                        = undef,
+    String $ssl_cert                   = '/etc/pki/dovecot/certs/dovecot.pem',
+    String $ssl_key                    = '/etc/pki/dovecot/private/dovecot.pem',
+    Optional[String] $ssl_cipher_list            = undef,
+    Optional[String] $ssl_protocols              = undef,
+    Optional[String] $ssl_dh_parameters_length   = undef,
+    String $ssl_dh                     = '/usr/share/dovecot/dh.pem',
     # 15-lda.conf
-    $postmaster_address         = undef,
-    $hostname                   = undef,
-    $lda_mail_plugins           = undef,
-    $lda_mail_location          = undef,
-    $lda_mailbox_autocreate     = undef,
-    $lda_mailbox_autosubscribe  = undef,
+    Optional[String] $postmaster_address         = undef,
+    Optional[String] $hostname                   = undef,
+    Optional[String] $lda_mail_plugins           = undef,
+    Optional[String] $lda_mail_location          = undef,
+    Optional[String] $lda_mailbox_autocreate     = undef,
+    Optional[String] $lda_mailbox_autosubscribe  = undef,
     # 15-mailboxes.conf.erb
-    $trash_mailbox_expunge_days = undef,
-    $junk_mailbox_expunge_days  = undef,
+    Optional[String] $trash_mailbox_expunge_days = undef,
+    Optional[String] $junk_mailbox_expunge_days  = undef,
     # 20-imap.conf
-    $imap_mail_plugins          = undef,
-    $imap_client_workarounds    = undef,
+    Optional[String] $imap_mail_plugins          = undef,
+    Optional[String] $imap_client_workarounds    = undef,
     # 20-lmtp.conf
-    $lmtp_mail_plugins          = undef,
-    $lmtp_save_to_detail_mailbox = false,
+    Optional[String] $lmtp_mail_plugins          = undef,
+    Boolean $lmtp_save_to_detail_mailbox = false,
     # 20-pop3.conf
-    $pop3_mail_plugins          = undef,
-    $pop3_uidl_format           = undef,
-    $pop3_client_workarounds    = undef,
+    Optional[String] $pop3_mail_plugins          = undef,
+    Optional[String] $pop3_uidl_format           = undef,
+    Optional[String] $pop3_client_workarounds    = undef,
     # 20-managesieve.conf
-    $manage_sieve               = undef,
-    $managesieve_service         = false,
-    $managesieve_service_count   = 0,
+    Boolean $manage_sieve               = false,
+    Boolean $managesieve_service         = false,
+    Integer $managesieve_service_count   = 0,
     # 90-sieve.conf
-    $sieve                      = '~/.dovecot.sieve',
-    $sieve_after                = undef,
-    $sieve_before               = undef,
-    $sieve_dir                  = '~/sieve',
-    $sieve_max_actions          = undef,
-    $sieve_max_redirects        = undef,
-    $sieve_max_script_size      = undef,
-    $sieve_quota_max_scripts    = undef,
-    $sieve_quota_max_storage    = undef,
-    $sieve_extensions            = undef,
-    $recipient_delimiter         = undef,
+    String $sieve                      = '~/.dovecot.sieve',
+    Optional[String] $sieve_after                = undef,
+    Optional[String] $sieve_before               = undef,
+    String $sieve_dir                  = '~/sieve',
+    Optional[String] $sieve_max_actions          = undef,
+    Optional[String] $sieve_max_redirects        = undef,
+    Optional[String] $sieve_max_script_size      = undef,
+    Optional[String] $sieve_quota_max_scripts    = undef,
+    Optional[String] $sieve_quota_max_storage    = undef,
+    Optional[String] $sieve_extensions            = undef,
+    Optional[String] $recipient_delimiter         = undef,
     # 90-plugin.conf
-    $fts                        = undef,
+    Optional[String] $fts                        = undef,
     # 90-quota.conf
-    $quota                      = undef,
-    $quota_warnings             = [],
+    Optional[String] $quota                      = undef,
+    Array $quota_warnings             = [],
     # auth-passwdfile.conf.ext
-    $auth_passwdfile_passdb     = undef,
-    $auth_passwdfile_userdb     = undef,
+    Optional[String] $auth_passwdfile_passdb     = undef,
+    Optional[String] $auth_passwdfile_userdb     = undef,
     # auth-sql.conf.ext
-    $auth_sql_userdb_static     = undef,
-    $auth_sql_path              = '/etc/dovecot/dovecot-sql.conf.ext',
+    Optional[String] $auth_sql_userdb_static     = undef,
+    String $auth_sql_path              = '/etc/dovecot/dovecot-sql.conf.ext',
     # auth-ldap.conf.ext
-    $auth_ldap_userdb_static    = undef,
-    $auth_master_separator      = '*',
-    $mail_max_userip_connections = 512,
-    $first_valid_uid             = false,
-    $last_valid_uid              = false,
+    Optional[String] $auth_ldap_userdb_static    = undef,
+    String $auth_master_separator      = '*',
+    Integer $mail_max_userip_connections = 512,
+    Boolean $first_valid_uid             = false,
+    Boolean $last_valid_uid              = false,
 
-    $manage_service              = true,
-    $custom_packages             = undef,
-    $ensure_packages             = 'installed',
+    Boolean $manage_service              = true,
+    Array $custom_packages             = [],
+    Optional[String] $ensure_packages             = 'installed',
 
-    $ldap_uris                   = undef,
-    $quota_enabled               = false,
-    $acl_enabled                 = false,
-    $replication_enabled         = false,
-    $shared_mailboxes            = false,
-    $options_plugins             = {},
+    Optional[String] $ldap_uris                   = undef,
+    Boolean $quota_enabled               = false,
+    Boolean $acl_enabled                 = false,
+    Boolean $replication_enabled         = false,
+    Boolean $shared_mailboxes            = false,
+    Hash $options_plugins             = {},
 ) {
-
-    validate_array($plugins)
-    # dovecot.conf
-    validate_string($protocols)
-    validate_string($listen)
-    validate_string($login_greeting)
-    validate_string($login_trusted_networks)
-
-
-    # 10-auth.conf
-    validate_bool($disable_plaintext_auth)
-    validate_string($auth_username_chars)
-    validate_string($auth_mechanisms)
-    validate_array($auth_include)
-    # 10-mail.conf
-    validate_string($mail_home)
-    validate_string($mail_location)
-    validate_string($mail_uid)
-    validate_string($mail_gid)
-    validate_string($mail_plugins)
-    # 10-master.conf
-    validate_string($default_process_limit)
-    validate_string($default_client_limit)
-    validate_string($auth_master_separator)
-    validate_string($auth_listener_userdb_mode)
-    validate_string($auth_listener_userdb_user)
-    validate_string($auth_listener_userdb_group)
-    validate_bool($auth_listener_postfix)
-    validate_string($auth_listener_postfix_mode)
-    validate_string($auth_listener_postfix_user)
-    validate_string($auth_listener_postfix_group)
-    validate_integer($imaplogin_imap_port)
-    validate_integer($imaplogin_imaps_port)
-    validate_bool($imaplogin_imaps_ssl)
-    validate_string($lmtp_unix_listener)
-    validate_string($lmtp_unix_listener_mode)
-    validate_string($lmtp_unix_listener_user)
-    validate_string($lmtp_unix_listener_group)
-    # 10-ssl.conf
-    validate_string($ssl)
-    validate_string($ssl_cert)
-    validate_string($ssl_key)
-    validate_string($ssl_key)
-    # 15-lda.conf
-    validate_string($postmaster_address)
-    validate_string($hostname)
-    validate_string($lda_mail_plugins)
-    validate_string($imap_mail_plugins)
-    # 20-lmtp.conf
-    validate_bool($lmtp_save_to_detail_mailbox)
-    validate_string($lmtp_mail_plugins)
-    # 20-managesieve.conf
-    validate_bool($managesieve_service)
-    validate_integer($managesieve_service_count)
-    # 90-sieve.conf
-    validate_string($sieve)
-    validate_string($sieve_dir)
-    validate_string($sieve_extensions)
-    validate_string($recipient_delimiter)
-    #Plugins
-    validate_string($ldap_uris)
-
-    validate_bool($quota_enabled)
-    validate_bool($acl_enabled)
-    validate_bool($shared_mailboxes)
-    validate_bool($replication_enabled)
-
-    validate_hash($options_plugins)
     if($replication_enabled) {
-        validate_hash($options_plugins[replication])
-        validate_string($options_plugins[replication][mail_replica])
-        validate_string($options_plugins[replication][dsync_remote_cmd])
-        validate_string($options_plugins[replication][replication_full_sync_interval])
-        validate_string($options_plugins[replication][replication_dsync_parameters])
+        assert_type(Hash, $options_plugins[replication])
+        assert_type(String, $options_plugins[replication][mail_replica])
+        assert_type(String, $options_plugins[replication][dsync_remote_cmd])
+        assert_type(String, $options_plugins[replication][replication_full_sync_interval])
+        assert_type(String, $options_plugins[replication][replication_dsync_parameters])
     }
 
     if $custom_packages == undef {
